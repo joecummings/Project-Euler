98f99ad0554b5b09b1bf671ea5d703b2038252e5
Joe Cummings <jrcummings27@gmail.com>
2017-12-22 09:46:04 -0500

    remove unnecessary files

--- linked_lists/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/joecummings/Downloads/eecs211-hw04
-# Build directory: /Users/joecummings/Downloads/eecs211-hw04/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Test_linked_test "linked_test")
-subdirs(.eecs211/lib/unittest-cpp)
-subdirs(.eecs211/lib/graphics)
